bf1184381a833d8d86c865194d30921bdfff0e5c
ThanakornDonklang <thanakorn.do@ku.th>
2021-10-29 21:08:36 +0700

    edit feature

--- src/components/home/Product.vue
+++ src/components/home/Product.vue
@@ -1,128 +1,145 @@
 <template>
-    <div class="mb-4" id="flex-content">
-        <img v-bind:src="picURL" class="mt-4" width="250" height="250" />
-        <h4 class="mt-3">{{ title }}</h4>
-        <h5 class="mt-1">Amount: {{ amount }}</h5>
-        <h5 class="mt-1">Price: {{ price.toFixed(2) }} Baht</h5>
-        <div class="d-grid gap-2 col-8 mx-auto">
-            <button
-                type="button"
-                class="btn btn-danger mb-4"
-                @click="addItemToCart"
-                v-if="!isAdmin()"
-            >
-                Add to Cart
-            </button>
-        </div>
+  <div class="mb-4" id="flex-content">
+    <img
+      src="../../../public/box.png"
+      class="mt-4"
+      width="250"
+      height="250"
+      v-if="isItemPicNull()"
+    />
+    <img
+      v-bind:src="picURL"
+      class="mt-4"
+      width="250"
+      height="250"
+      v-if="!isItemPicNull()"
+    />
+    <h4 class="mt-3">{{ title }}</h4>
+    <h5 class="mt-1">Amount: {{ amount }}</h5>
+    <h5 class="mt-1">Price: {{ price.toFixed(2) }} Baht</h5>
+    <div class="d-grid gap-2 col-8 mx-auto">
+      <button
+        type="button"
+        class="btn btn-danger mb-4"
+        @click="addItemToCart"
+        v-if="!isAdmin()"
+      >
+        Add to Cart
+      </button>
     </div>
+  </div>
 </template>
 
 <script>
-import ShopStore from '@/store/Shop';
-import userCart from '@/store/Cart';
+import ShopStore from "@/store/Shop";
+import userCart from "@/store/Cart";
 
 export default {
-    name: 'Item',
-    props: ['item'],
-    data() {
-        return {
-            currentUser: '',
-            placeholder: 'https://via.placeholder.com/250x250',
-            title: '',
-            amount: '',
-            price: '',
-            picURL: '',
-        };
+  name: "Item",
+  props: ["item"],
+  data() {
+    return {
+      currentUser: "",
+      placeholder: "https://via.placeholder.com/250x250",
+      title: "",
+      amount: "",
+      price: "",
+      picURL: "",
+    };
+  },
+  created() {
+    this.validProductData();
+    // this.fetchCurrentUserdata();
+  },
+  methods: {
+    isAuthen() {
+      return ShopStore.getters.isAuthen;
     },
-    created() {
-        this.validProductData();
-        // this.fetchCurrentUserdata();
-    },
-    methods: {
-        isAuthen() {
-            return ShopStore.getters.isAuthen;
-        },
 
-        addItemToCart() {
-            userCart.commit('addItemInCart', this.item);
-            this.$swal(`เพิ่มสินค้าสำเร็จ`, `เพิ่ม ${this.title} 1 จำนวนลงใน Cart`, 'success');
-        },
-        isAdmin() {
-            return ShopStore.getters.isAuthen ? ShopStore.getters.isAdmin : false;
-        },
-        // Check If image exists
-        checkImage(url) {
-            return axios.get(url).then(result => {
-                url = this.endPoint + url;
-                return result.status == 200 ? true : false;
-            });
-        },
-        async getValidImageUrl(url) {
-            return (await this.checkImage(url)) ? url : this.placeholder;
-        },
-        async validProductData() {
-            this.title = this.item.name || 'Item TITLE';
-            this.price = this.item.price ? this.item.price : 'Item PRICE';
-            this.amount = this.item.amount ? this.item.amount : 'Item AMOUNT';
-            this.picURL = this.item.image_path
-                ? // edit url to product image
-                  await this.getValidImageUrl(this.product.photo.formats.small.url)
-                : this.placeholder;
-        },
-        fetchCurrentUserdata() {
-            this.currentUser = ShopStore.getters.getCurrentUser;
-        },
-        isItemPicNull() {
-            return this.item.image_path === null;
-        },
-        buyItem() {
-            console.log('THIS IS FROM BUY ITEM FUNCTION');
-        },
-        edit(item) {
-            this.$router.push({
-                name: 'EditItem',
-                params: { id: item.id },
-            });
-        },
+    addItemToCart() {
+      userCart.commit("addItemInCart", this.item);
+      this.$swal(
+        `เพิ่มสินค้าสำเร็จ`,
+        `เพิ่ม ${this.title} 1 จำนวนลงใน Cart`,
+        "success"
+      );
     },
+    isAdmin() {
+      return ShopStore.getters.isAuthen ? ShopStore.getters.isAdmin : false;
+    },
+    // Check If image exists
+    checkImage(url) {
+      return axios.get(url).then((result) => {
+        url = this.endPoint + url;
+        return result.status == 200 ? true : false;
+      });
+    },
+    async getValidImageUrl(url) {
+      return (await this.checkImage(url)) ? url : this.placeholder;
+    },
+    async validProductData() {
+      this.title = this.item.name || "Item TITLE";
+      this.price = this.item.price ? this.item.price : "Item PRICE";
+      this.amount = this.item.amount ? this.item.amount : "Item AMOUNT";
+      this.picURL = this.item.image_path
+        ? // edit url to product image
+          await this.getValidImageUrl(this.product.photo.formats.small.url)
+        : this.placeholder;
+    },
+    fetchCurrentUserdata() {
+      this.currentUser = ShopStore.getters.getCurrentUser;
+    },
+    isItemPicNull() {
+      return this.item.image_path === null;
+    },
+    buyItem() {
+      console.log("THIS IS FROM BUY ITEM FUNCTION");
+    },
+    edit(item) {
+      this.$router.push({
+        name: "EditItem",
+        params: { id: item.id },
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Questrial&display=swap");
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 #flex {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 #flex-content {
-    position: relative;
-    width: 29%;
-    margin-left: 4%;
-    text-align: center;
-    padding-top: 10px;
-    font-family: 'Questrial', sans-serif;
-    cursor: pointer;
+  position: relative;
+  width: 29%;
+  margin-left: 4%;
+  text-align: center;
+  padding-top: 10px;
+  font-family: "Questrial", sans-serif;
+  cursor: pointer;
 }
 #flex-content:hover {
-    transform: scale(1.05);
-    transition: 0.25s;
-    box-shadow: 5px 5px rgba(0, 0, 0, 0.6);
+  transform: scale(1.05);
+  transition: 0.25s;
+  box-shadow: 5px 5px rgba(0, 0, 0, 0.6);
 }
 
 #flex-content::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0.9;
-    z-index: -1;
-    background-color: white;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.9;
+  z-index: -1;
+  background-color: white;
 }
 </style>
--- src/store/Shop.js
+++ src/store/Shop.js
@@ -1,98 +1,105 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
-import AuthService from '@/services/AuthService';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import AuthService from "@/services/AuthService";
 
-const end_point = process.env.SHOP_VUE_APP_SHOP_ENDPOINT || 'http://localhost:8000';
+const end_point =
+  process.env.SHOP_VUE_APP_SHOP_ENDPOINT || "http://localhost:8000";
 
 Vue.use(Vuex);
 
-let auth_key = 'auth-shop';
+let auth_key = "auth-shop";
 let auth = JSON.parse(localStorage.getItem(auth_key));
 const initialStateUser = {
-    user: auth ? auth.user : '',
-    jwt: auth ? auth.jwt : '',
-    isAuthen: auth ? true : false,
+  user: auth ? auth.user : "",
+  jwt: auth ? auth.jwt : "",
+  isAuthen: auth ? true : false,
 };
 
 export default new Vuex.Store({
-    state: {
-        currentUser: initialStateUser,
-        endPoint: end_point,
-        items: [],
-        editItem: {},
+  state: {
+    currentUser: initialStateUser,
+    endPoint: end_point,
+    items: [],
+    editItem: {},
+  },
+  getters: {
+    currentUser: (state) => state.currentUser,
+    items: (state) => state.items,
+    editItem: (state) => state.editItem,
+    endPoint: (state) => state.endPoint,
+    isAuthen: (state) => state.currentUser.isAuthen,
+    isAdmin: (state) => state.currentUser.user.role == "admin",
+  },
+  mutations: {
+    setCurrentUser(state, user, jwt) {
+      state.currentUser.user = user;
+      state.currentUser.jwt = jwt;
+      state.currentUser.isAuthen = true;
     },
-    getters: {
-        currentUser: state => state.currentUser,
-        items: state => state.items,
-        editItem: state => state.editItem,
-        endPoint: state => state.endPoint,
-        isAuthen: state => state.currentUser.isAuthen,
-        isAdmin: state => state.currentUser.user.role == 'admin',
+
+    resetCurrentUser(state) {
+      state.currentUser.user = "";
+      state.currentUser.jwt = "";
+      state.currentUser.isAuthen = false;
     },
-    mutations: {
-        setCurrentUser(state, user, jwt) {
-            state.currentUser.user = user;
-            state.currentUser.jwt = jwt;
-            state.currentUser.isAuthen = true;
-        },
 
-        resetCurrentUser(state) {
-            state.currentUser.user = '';
-            state.currentUser.jwt = '';
-            state.currentUser.isAuthen = false;
-        },
-
-        setItems(state, res) {
-            state.items = res;
-        },
-        getItemById(state, res) {
-            state.editItem = res;
-        },
+    setItems(state, res) {
+      state.items = res;
     },
-    actions: {
-        async register({ commit }, body) {
-            let res = await AuthService.register(body);
-            if (res.success) {
-                commit('setCurrentUser', res.user, res.jwt);
-            }
-            return res;
-        },
-
-        async login({ commit }, body) {
-            let res = await AuthService.login(body);
-            if (res.success) {
-                commit('setCurrentUser', res.user, res.jwt);
-            }
-            return res;
-        },
-
-        async logout({ commit }) {
-            AuthService.logout();
-            commit('resetCurrentUser');
-        },
-
-        async fetchItems({ commit }) {
-            try {
-                let res = await axios.get(end_point + '/api/items');
-                commit('setItems', res.data);
-                return res.data;
-            } catch (error) {
-                console.error('Fetch Items error');
-                console.error(error);
-            }
-        },
-        async addItem({ commit }, payload) {
-            let url = `${end_point}/api/items/add`;
-            let body = payload;
-
-            let res = await axios.post(url, body);
-        },
-        async getItemById({ commit }, id) {
-            let url = `${end_point}/api/items/${id}`;
-            let res = await axios.get(url);
-            commit('getItemById', res.data);
-        },
+    getItemById(state, res) {
+      state.editItem = res;
     },
-    modules: {},
+  },
+  actions: {
+    async register({ commit }, body) {
+      let res = await AuthService.register(body);
+      if (res.success) {
+        commit("setCurrentUser", res.user, res.jwt);
+      }
+      return res;
+    },
+
+    async login({ commit }, body) {
+      let res = await AuthService.login(body);
+      if (res.success) {
+        commit("setCurrentUser", res.user, res.jwt);
+      }
+      return res;
+    },
+
+    async logout({ commit }) {
+      AuthService.logout();
+      commit("resetCurrentUser");
+    },
+
+    async fetchItems({ commit }) {
+      try {
+        let res = await axios.get(end_point + "/api/items");
+        commit("setItems", res.data);
+        return res.data;
+      } catch (error) {
+        console.error("Fetch Items error");
+        console.error(error);
+      }
+    },
+    async addItem({ commit }, payload) {
+      let url = `${end_point}/api/items/add`;
+      let body = payload;
+
+      let res = await axios.post(url, body);
+    },
+    async getItemById({ commit }, id) {
+      let url = `${end_point}/api/items/${id}`;
+      let res = await axios.get(url);
+      commit("getItemById", res.data);
+    },
+    async editItem({ commit }, payload) {
+      let url = `${end_point}/api/items/edit/${payload.id}`;
+      let body = payload;
+
+      let res = await axios.put(url, body);
+    },
+  },
+  modules: {},
 });
